5b8e479ba76bac1040d58f05a0a8b35fd6aa649d
rockneters <89226199+rockneters@users.noreply.github.com>
2021-09-07 11:55:20 +0700

    Add files via upload

---
+++ ohp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ohp-db.sh;chmod +x ohp-db.sh;./ohp-db.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ohp-ovpn.sh;chmod +x ohp-ovpn.sh;./ohp-ovpn.sh
