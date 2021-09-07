bb9a94932d7f352867894875480fbd5bb4973431
rockneters <89226199+rockneters@users.noreply.github.com>
2021-09-07 11:51:54 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#         Premium Auto Script By Ceu Edoh       #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
+echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
+echo -e "# Build Up By Kang Coly                         #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Thanks To                                     #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# CEU POPON, CEU BIAH, CEU ODAH                 #" | lolcat
+echo -e "# Loteknya enak pisan jadi makan tiap hari      #" | lolcat
+echo -e "# Tanks to:                                     #" | lolcat
+echo -e "# Emak dan Abah                                 #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "=================================================" | lolcat
